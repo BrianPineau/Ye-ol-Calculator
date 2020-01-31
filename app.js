f2c7351790059b7a5c43643c95794cb7c6abc2a2
Brian Pineau <brianpineau@Brians-iMac.local>
2020-01-30 20:27:02 -0600

    Switch statement for operators created

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-var dispNum, numBtn;
+let dispNum, pendingVal, numBtn, calcBtn, clearBtn, dispString, btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9;
 
 dispNum = document.getElementById('display-output');
 
@@ -16,26 +16,72 @@ btn9 = document.querySelector('.num-btn-9');
 numBtn = document.getElementsByClassName('num-btn');
 calcBtn = document.getElementsByClassName('calc-btn');
 
+clearBtn = document.getElementById('clear-scrn');
+
 // Event Listeners
 for (let i = 0; i < numBtn.length; i++) {
     numBtn[i].addEventListener('click', displayNum, false);
 };
 
-
-/*
 for (let i = 0; i < calcBtn.length; i++) {
     calcBtn[i].addEventListener('click', doCalculation); 
 };
-*/
 
+clearBtn.addEventListener('click', clearDisplay);
+
+//  Output number to display
 dispVal = '0';
+pendingVal;
+dispString = [];
 
 function displayNum (e) {
     btnText = e.target.innerText;
+
+    if (dispVal === '0') {
+        dispVal = '';
+    }
+
     dispVal += btnText;
+    dispString.push(dispVal);
+    console.log(dispString);
     dispNum.innerText = dispVal;
+};
+
+function clearDisplay () {
+    dispNum.innerText = '0';
+    dispVal = '';
+    dispString = [];
 }
 
+//  Do calculations
+function doCalculation (e) {
+    let operator = e.target.innerText;
+
+    switch (operator) {
+        case '+':
+            pendingVal = dispVal;
+            dispVal = '0';
+            dispString.push('+');
+            dispNum.innerText = dispString;
+            break;
+        case '-':
+
+            break;
+        case '*':
+
+            break;
+        case '÷':
+
+            break;
+        case '=':
+            dispString.push(dispVal)
+            evaluation = eval(dispString.join(' '));
+            dispVal = evaluation + '';
+            
+        default:
+            break;
+    }
+}
 
 
 
